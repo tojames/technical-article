31e46b822a99cf6d6635f98f9fed7997bc0c0ad0
aatoe <2503539119@qq.com>
2021-06-12 00:24:27 +0800

    上下文

--- 前端面试题训练营「20210517」/20210517/4.js
+++
@@ -1,8 +0,0 @@
-/*
- * 数据通信机制：ajax/axios/fetch实操及优化 
- *   + ajax、axios、fetch三者的区别
- *   + axios二次封装
- *   + 课后思考：fetch的二次封装处理
- * http://www.axios-js.com/zh-cn/docs/
- * https://developer.mozilla.org/zh-CN/docs/Web/API/Fetch_API/Using_Fetch
- */
