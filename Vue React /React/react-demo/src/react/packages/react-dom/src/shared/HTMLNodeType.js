0f6888b4e6a96de2f2aec2de053978494f2c6526
aatoe <2503539119@qq.com>
2022-02-27 14:32:40 +0800

    feat: react-dom 源码解读2

---
+++ Vue React /React/react-demo/src/react/packages/react-dom/src/shared/HTMLNodeType.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+/**
+ * HTML nodeType values that represent the type of the node
+ */
+
+export const ELEMENT_NODE = 1; // 表示元素，body、a、div
+export const TEXT_NODE = 3; // 文字内容 <div>描述是这里面的内容</div>
+export const COMMENT_NODE = 8; // 注释
+export const DOCUMENT_NODE = 9; // document 节点
+export const DOCUMENT_FRAGMENT_NODE = 11; // framenet 节点
